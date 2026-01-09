ec1c5f195cfd2b2cd14e15116d78b87c9e4cf25b
drkup <darrenkwondev46@gmail.com>
2026-01-09 19:48:27 +0900

    feat: cli for block gen, tx gen and print it

---
+++ lib/src/bin/tx_print.rs
@@ -0,0 +1,19 @@
+use btclib::types::Transaction;
+use btclib::util::Savable;
+use std::env;
+use std::fs::File;
+use std::process::exit;
+
+fn main() {
+    let path = if let Some(arg) = env::args().nth(1) {
+        arg
+    } else {
+        eprintln!("Usage: tx_print <tx_file>");
+        exit(1);
+    };
+
+    if let Ok(file) = File::open(path) {
+        let block = Transaction::load(file).expect("Failed to load block");
+        println!("{:#?}", block);
+    }
+}
